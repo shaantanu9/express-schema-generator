58f51b682b7745c32b9d0a85fe2925156ce66c8c
shantanu <shantanubombatkar2@gmail.com>
2022-02-21 10:22:13 +0530

    update

--- samples/middlewares/authenticate.js
+++ samples/middlewares/authenticate.js
@@ -1,10 +1,13 @@
-require("dotenv").config();
+require("dotenv").config;
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (token) => {
   return new Promise((resolve, reject) => {
+    
+    console.log(process.env.JWT_SECRET_KEY)
+
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-      if (err) return reject(err);
+      if (err) return console.log(err);
 
       resolve(user);
     });
@@ -12,37 +15,17 @@ const verifyToken = (token) => {
 };
 
 module.exports = async (req, res, next) => {
-  // check if authorization header has been set
-  // if not throw an errors
-  if (!req.headers.authorization)
-    return res.status(400).send({
-      message: "authorization token was not provided or was not valid",
-    });
+  if (!req.headers.authorization) return res.status(403).send("Token not found");
 
-  // if bearer token is in authorization header
-  // if not throw an error
   if (!req.headers.authorization.startsWith("Bearer "))
-    return res.status(400).send({
-      message: "authorization token was not provided or was not valid",
-    });
+    return res.status(403).send("Token not found");
 
-  // split the bearer token and get the [1] which is the token
   const token = req.headers.authorization.split(" ")[1];
 
-  // then we will call jwt to verify the token
-  let user;
-  // if token is invalid then we will throw an error
-  try {
-    user = await verifyToken(token);
-  } catch (err) {
-    return res.status(400).send({
-      message: "authorization token was not provided or was not valid",
-    });
-  }
+  const user = await verifyToken(token);
 
-  // if token is valid then we will put the user retrieved from the token in the req object
-  req.user = user.user;
+    req.body.user_id = user.user._id
 
-  // return next()
+  console.log(req.user, "authorization");
   return next();
 };
