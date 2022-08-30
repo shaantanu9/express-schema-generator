2126461fd7f2a946bbf43087e2c4b71b7b477e92
Shantanu Bombatkar <33876955+shaantanu9@users.noreply.github.com>
2022-08-30 22:35:15 +0530

    Edited Model File

--- samples/models/user.model.js
+++ samples/models/user.model.js
@@ -7,7 +7,7 @@ const UserSchema = mongoose.Schema({
         "last_name": {type:String,required:true},
         "email": {type:String,required:true},
         "gender": {type:String,required:true},
-        "address": {type:String,required:true}
+        "address": {type:String,required:true},
         "pic": {type:String}
 })
 
